ced20cc0876c509843b98da7f2bd25674044974e
MKD <2424618223@qq.com>
2026-06-19 23:35:54 +0800

    Update template source and modular inverse notes

--- doc/scripts/test-compiles.sh
+++ doc/scripts/test-compiles.sh
@@ -1,7 +1,7 @@
 mkdir -p build
 FILE="$1"
 echo "
-#include \"../content/contest/template.h\"
+#include \"../content/contest/template.cpp\"
 #include \"../$FILE\"
 " >build/temp.cpp
 g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++17 build/temp.cpp -o build/test-compiles.exe && rm -f build/test-compiles.exe build/temp.cpp
