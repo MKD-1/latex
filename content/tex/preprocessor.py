2b94b6c2e024a1f6726d8e382ea744e5b97940cb
MKD <2424618223@qq.com>
2026-06-21 14:19:37 +0800

    Remove PDF build artifacts and ignore latexmk files

--- content/tex/preprocessor.py
+++ content/tex/preprocessor.py
@@ -68,7 +68,7 @@ def find_start_comment(source, start=None):
     return first
 
 def processwithcomments(caption, instream, outstream, listingslang):
-    knowncommands = ['Author', 'Date', 'Description', 'DescriptionEn', 'Source', 'Time', 'Memory', 'License', 'Status', 'Usage', 'Details']
+    knowncommands = ['Author', 'Date', 'Description', 'enDescription', 'Source', 'Time', 'Memory', 'License', 'Status', 'Usage', 'Details']
     requiredcommands = ['Author', 'Description']
     includelist = []
     error = ""
