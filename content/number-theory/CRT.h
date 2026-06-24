e4e6f370aa383b3f7e93551a6a79957fa2c28c1b
MKD <2424618223@qq.com>
2026-06-24 21:26:49 +0800

    Fix lecture body blank detection

--- content/number-theory/CRT.h
+++ content/number-theory/CRT.h
@@ -2,11 +2,16 @@
  * Author: Simon Lindholm
  * Date: 2019-05-22
  * License: CC0
- * Description: Chinese Remainder Theorem.
+ * enDescription: Chinese Remainder Theorem.
  *
  * \texttt{crt(a, m, b, n)} computes $x$ such that $x\equiv a \pmod m$, $x\equiv b \pmod n$.
  * If $|a| < m$ and $|b| < n$, $x$ will obey $0 \le x < \text{lcm}(m, n)$.
  * Assumes $mn < 2^{62}$.
+ * Description: 中国剩余定理。
+ * 
+ * \texttt{crt(a, m, b, n)} 计算 $x$，使得 $x\equiv a \pmod m$, $x\equiv b \pmod n$。
+ * 如果 $|a| < m$ 且 $|b| < n$， 则 $x$ 将满足 $0 \le x < \text{lcm}(m, n)$。
+ * 假设 $mn < 2^{62}$。
  * Time: $\log(n)$
  * Status: Works
  */
--- content/number-theory/euclid.h
+++ content/number-theory/euclid.h
@@ -2,9 +2,12 @@
  * Author: Unknown
  * Date: 2002-09-15
  * Source: predates tinyKACTL
- * Description: Finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
+ * enDescription: Finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
  * you just need gcd, use the built in \texttt{\_\_gcd} instead.
  * If $a$ and $b$ are coprime, then $x$ is the inverse of $a \pmod{b}$.
+ * Description: 找到两个整数 $x$ 和 $y$，使得 $ax+by=\gcd(a,b)$。如果你只需要 gcd，可以使用内置的 \texttt{\_\_gcd}。
+ * 如果 $a$ 和 $b$ 互质，那么 $x$ 就是 $a \pmod{b}$ 的逆元。
+ * Time: O(\log\min(a,b))
  */
 #pragma once
 
