5a4957893d07bb8efe54a27605ccf90d4a053d54
MKD-1 <2424618223@qq.com>
2026-09-03 09:19:35 +0800

    修改bellmanFord

--- content/graph/BellmanFord.h
+++ content/graph/BellmanFord.h
@@ -6,7 +6,7 @@
  * enDescription: Calculates shortest paths from $s$ in a graph that might have negative edge weights.
  * Unreachable nodes get dist = inf; nodes reachable through negative-weight cycles get dist = -inf.
  * Assumes $V^2 \max |w_i| < \tilde{} 2^{63}$.
- * Description: 计算可能含$\textbf{负权边}$的图中从 s 出发的最短路。
+ * Description: 单源可能含$\textbf{负权边}$最短路。
  * 不可达点的距离为 inf；能经由负权环到达的点距离为 -inf。
  * 假设 $V^2 \max |w_i| < \tilde{} 2^{63}$。
  * Time: O(VE)
@@ -32,8 +32,8 @@ void bellmanFord(vector<Node>& nodes, vector<Ed>& eds, int s) {
       dest.dist = (i < lim-1 ? d : -infl);
     }
   }
-  rep(i,0,lim) for (Ed e : eds) {
-    if (nodes[e.u].dist == -infl)
-      nodes[e.v].dist = -infl;
+  rep(i,0,lim) for (Ed ed : eds) {
+    if (nodes[ed.u].dist == -infl)
+      nodes[ed.v].dist = -infl;
   }
 }
